b5a61ea675f27ae2d0c8ee53e084af6b37d936e3
Riyaz <r-ahmed@riysaim.com>
2022-02-25 18:08:34 +0530

    added event listener

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-function loadDiv()
+document.addEventListener("click", function(event)
 {
-
-}
+    let id=event.target
+    if(id.id=="feature"){
+        alert("hello feature")
+    } else {
+      {
+        if(id.id=="h-work")
+        {
+          alert("hello work")
+        }
+        else {
+          {
+            alert("hello pricing")
+          }
+        }
+      }
+    }
+})
