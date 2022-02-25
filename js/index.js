d4740bb127fbc95bbcf61e2b96953cf136a7303e
Riyaz <r-ahmed@riysaim.com>
2022-02-25 19:48:03 +0530

    added event listener

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ document.querySelector("#nav-bar").addEventListener("click", function(event)
     let id=event.target
     if(id.id=="feature")
        {
-         #.container.load("#feature");
+         #container.load("#feature");
         } else {
           if(id.id=="h-work")
                  {
